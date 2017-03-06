c6c6e27a780a3b2140144d8f830217fc574aba26
AdrianAndrearczyk <s12622@pjwstk.edu.pl>
2017-03-07 00:17:47 +0100

    Dodanie obslugi bledow, implementacji i asercji

--- zad1/src/main/java/Vector.java
+++ zad1/src/main/java/Vector.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public interface Vector {
 
-    public List<Double> add(List<Double> w1, List<Double> w2) throws VectorException;
+    public List<Double> addVectors(List<Double> w1, List<Double> w2) throws VectorException;
     public void set(List<Double> lista);
     public List<Double> get();
 
--- zad1/src/test/java/VectorTests.java
+++ zad1/src/test/java/VectorTests.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.*;
 import org.junit.runner.*;
 import org.junit.runners.JUnit4;
 
@@ -12,26 +11,62 @@ import org.junit.runners.JUnit4;
 @RunWith ( JUnit4. class )
 public class VectorTests {
 
+    Vector wektor = new VectorImpl();
+
+
     @org.junit.Test
-    public void testVector(){
+    public void firstVectorTest() {
         List<Double> wektor1 = new ArrayList<Double>();
         List<Double> wektor2 = new ArrayList<Double>();
         List<Double> wektor3 = new ArrayList<Double>();
 
-        wektor1.add(5.62);
-        wektor1.add(3.14);
-        wektor1.add(2.30);
+        wektor1.add(3.5);
+        wektor1.add(1.2);
+        wektor1.add(3.3);
 
-        wektor2.add(5.12);
-        wektor2.add(3.29);
-        wektor2.add(7.51);
+        wektor2.add(14.5);
+        wektor2.add(9.5);
+        wektor2.add(3.2);
 
         //suma wektorów wektor1, wektor2
-        wektor3.add(10.74);
-        wektor3.add(6.43);
-        wektor3.add(9.81);
+        wektor3.add(18.0);
+        wektor3.add(10.7);
+        wektor3.add(6.5);
 
-        assertEquals(wektor3, vector.add(wektor1, wektor2));
-
+        try {
+            assertEquals(wektor3, wektor.addVectors(wektor1, wektor2));
+        } catch (VectorException e) {
+//            e.printStackTrace();
+            assertFalse(true);
+        }
 
     }
+
+    @org.junit.Test
+    public void secondVectorTest() {
+        List<Double> wektor1 = new ArrayList<Double>();
+        List<Double> wektor2 = new ArrayList<Double>();
+        List<Double> wektor3 = new ArrayList<Double>();
+
+        wektor1.add(2.1);
+        wektor1.add(4.2);
+        wektor1.add(3.3);
+
+        wektor2.add(12.5);
+        wektor2.add(3.5);
+        wektor2.add(6.2);
+
+        //suma wektorów wektor1, wektor2
+        wektor3.add(14.6);
+        wektor3.add(7.7);
+        wektor3.add(9.5);
+
+        try {
+            assertEquals(wektor3, wektor.addVectors(wektor1, wektor2));
+        } catch (VectorException e) {
+//            e.printStackTrace();
+            assertFalse(true);
+        }
+
+    }
+}
